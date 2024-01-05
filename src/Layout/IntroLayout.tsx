0aa3d02a0e8d3bd22d2765906486f41bcf067739
fjrfathurrahman <fjrfathurrahman25@gmail.com>
2024-01-05 08:44:49 +0700

    Perbaiki Modal Component v3

--- src/Layout/IntroLayout.tsx
+++ src/Layout/IntroLayout.tsx
@@ -1,13 +1,19 @@
 import { headline, pp, subHeadline } from "../data/Data";
 import { ButtonCom } from "../components/button/ButtonCom";
-import { useDisclosure, Modal, ModalBody, ModalContent, ModalHeader } from "@nextui-org/react";
+import {
+  useDisclosure,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+} from "@nextui-org/react";
 
 export default function IntroLayout() {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const handleOpen = () => {
     onOpen();
-  }
+  };
 
   return (
     <main className="py-12">
@@ -24,41 +30,42 @@ export default function IntroLayout() {
         />
       </div>
       <div>
-        <img src={pp} className="rounded-full w-52 h-52 mx-auto shadowCostum border-8 border-neutral-100" />
+        <img
+          src={pp}
+          className="rounded-full w-52 h-52 mx-auto shadowCostum border-8 border-neutral-100"
+        />
       </div>
-      <ModalCom isOpen={isOpen} onOpenChange={onOpenChange}/>
+      <ModalCom isOpen={isOpen} onOpenChange={onOpenChange} />
     </main>
   );
 }
 
 const ModalCom = (props: any) => {
   return (
-    <>
-      <Modal
-        isOpen={props.isOpen}
-        placement={"center"}
-        onOpenChange={props.onOpenChange}
-      >
-        <ModalContent className="bg-[#fffffe]">
-          {() => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">
-                <h1 className="font-bold">Happy Birthday my love</h1>
-              </ModalHeader>
-              <ModalBody>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Cumque similique natus soluta, iste nulla tenetur vitae
-                  laboriosam nihil maxime ullam ad, voluptate expedita dolores
-                  tempore aliquam. Corrupti distinctio quas consectetur quam
-                  sapiente, veniam consequuntur nisi? Nulla ducimus modi
-                  quibusdam illo?
-                </p>
-              </ModalBody>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
+    <Modal
+      isOpen={props.isOpen}
+      placement={"center"}
+      onOpenChange={props.onOpenChange}
+      className="h-20"
+    >
+      <ModalContent className="bg-[#fffffe]">
+        {() => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              Happy Birthday my love
+            </ModalHeader>
+            <ModalBody>
+              <p className="text-justify">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
+                similique natus soluta, iste nulla tenetur vitae laboriosam
+                nihil maxime ullam ad, voluptate expedita dolores tempore
+                aliquam. Corrupti distinctio quas consectetur quam sapiente,
+                veniam consequuntur nisi? Nulla ducimus modi quibusdam illo?
+              </p>
+            </ModalBody>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
   );
 };
